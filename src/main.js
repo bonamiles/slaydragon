19947579445e798a1d6543686194325978725036
Eva <bonamiles@gmail.com>
2023-08-21 17:16:35 +0530

    Hero and About Section

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 import '../style/normalise.css'
 import '../style/style.css'
+import '../style/components/header.css'
+import '../style/components/hero.css'
+import '../style/components/about.css'
 import '../style/utils.css'
